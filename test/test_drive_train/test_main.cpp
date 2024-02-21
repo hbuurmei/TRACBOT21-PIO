56beadf6444b3d87d309aaf9ae8c318dd8132b42
ndelurgio <delurgion@gmail.com>
2024-02-20 23:46:14 -0800

    working test for forward motor drive

--- test/test_drive_train/test_main.cpp
+++ test/test_drive_train/test_main.cpp
@@ -6,8 +6,10 @@
 #define EnB     6
 
 #include <Arduino.h>
+#include <unity.h>
 
 void setup() {
+  UNITY_BEGIN(); // IMPORTANT LINE!
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(IN1,OUTPUT);
@@ -16,12 +18,20 @@ void setup() {
   pinMode(IN4,OUTPUT);
   pinMode(EnA,OUTPUT);
   pinMode(EnB,OUTPUT);
-  // Configure Pin 1
+  // Configure Motor 2
   digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,HIGH);
-  analogWrite(EnA,50);
+  digitalWrite(IN2,LOW);
+  analogWrite(EnA,200);
+  // Configure Motor 2
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  analogWrite(EnB,200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  Serial.print("Hello");
+  if (Serial.available()) {UNITY_END();}
+  
 }
