1e8d1f93ed5d11e74c402db2b5be7a3593d11b26
ndelurgio <delurgion@gmail.com>
2024-02-25 16:17:11 -0800

    better imu processing script

--- test/test_imu.cpp
+++ test/test_imu.cpp
@@ -1,69 +1,187 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include "SparkFun_BMI270_Arduino_Library.h"
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
-// Create a new sensor object
-BMI270 imu;
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 
-// I2C address selection
-uint8_t i2cAddress = BMI2_I2C_PRIM_ADDR; // 0x68
-//uint8_t i2cAddress = BMI2_I2C_SEC_ADDR; // 0x69
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+//MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
 
-void setup()
-{
-    // Start serial
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define OUTPUT_IMU_SETTINGS
+
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
+// #define OUTPUT_READABLE_ACCELGYRO
+
+// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
+// binary, one right after the other. This is very fast (as fast as possible
+// without compression or data loss), and easy to parse, but impossible to read
+// for a human.
+//#define OUTPUT_BINARY_ACCELGYRO
+
+// For un
+
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
     Serial.begin(9600);
-    Serial.println("BMI270 Example 1 - Basic Readings I2C");
 
-    // Initialize the I2C library
-    Wire.begin();
+    // initialize device
+    // Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
 
-    // Check if sensor is connected and initialize
-    // Address is optional (defaults to 0x68)
-    while(imu.beginI2C(i2cAddress) != BMI2_OK)
-    {
-        // Not connected, inform user
-        Serial.println("Error: BMI270 not connected, check wiring and I2C address!");
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-        // Wait a bit to see if connection is established
-        delay(1000);
-    }
+    // use the code below to change accel/gyro offset values
+    /*
+    Serial.println("Updating internal sensor offsets...");
+    // -76	-2359	1688	0	0	0
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    accelgyro.setXGyroOffset(220);
+    accelgyro.setYGyroOffset(76);
+    accelgyro.setZGyroOffset(-85);
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    */
 
-    Serial.println("BMI270 connected!");
+    // configure Arduino LED pin for output
+    pinMode(LED_PIN, OUTPUT);
+
+    #ifdef OUTPUT_IMU_SETTINGS
+        Serial.println(accelgyro.getFullScaleAccelRange());
+        Serial.println(accelgyro.getFullScaleGyroRange());
+    #endif
 }
 
-void loop()
-{
-    // Get measurements from the sensor. This must be called before accessing
-    // the sensor data, otherwise it will never update
-    imu.getSensorData();
 
-    // Print acceleration data
-    Serial.print("Acceleration in g's");
-    Serial.print("\t");
-    Serial.print("X: ");
-    Serial.print(imu.data.accelX, 3);
-    Serial.print("\t");
-    Serial.print("Y: ");
-    Serial.print(imu.data.accelY, 3);
-    Serial.print("\t");
-    Serial.print("Z: ");
-    Serial.print(imu.data.accelZ, 3);
+// enum IMU_ACCEL_RANGE {
+//     PLUSMINUS_2G,
+//     PLUSMINUS_4G,
+//     PLUSMINUS_8G,
+//     PLUSMINUS_16G,
+// }
+class IMU {
+    public:
+        IMU(MPU6050 imu, float g = 9.81) {
+            g = g;
+            update_config(imu);
+        }
+        void update_config(MPU6050 imu) {
+            int8_t accel_config = imu.getFullScaleAccelRange();
+            switch (accel_config) {
+                case 0: //+-2g
+                    accel_raw_to_g = 1/16384.;
+                case 1: //+-4g
+                    accel_raw_to_g = 1/8192.;
+                case 2: //+-8g
+                    accel_raw_to_g = 1/4096.;
+                case 3: //+-16g
+                    accel_raw_to_g = 1/2048.;
+            }
+            int8_t gyro_config = imu.getFullScaleGyroRange();
+            switch (gyro_config) {
+                case 0: //+-250deg/s
+                    gyro_raw_to_dps = 1/131.;
+                case 1: //+-500deg/s
+                    gyro_raw_to_dps = 1/65.5;
+                case 2: //+-1000deg/s
+                    gyro_raw_to_dps = 1/32.8;
+                case 3: //+-2000deg/s
+                    gyro_raw_to_dps = 1/16.4;
+            }
+        }
+        void update_measurement() {
+            
+        }
+        float accelX; // meters/sec^2
+        float accelY; // meters/sec^2
+        float accelZ; // meters/sec^2
+        float gyroX;  // rad/sec
+        float gyroY;  // rad/sec
+        float gyroZ;  // rad/sec
+    private:
+        MPU6050 imu;
+        const float g = 9.81;
+        int16_t accelX_raw; // int16_t from min to max range
+        int16_t accelY_raw; // int16_t from min to max range
+        int16_t accelZ_raw; // int16_t from min to max range
+        int16_t gyroX_raw; // int16_t from min to max range
+        int16_t gyroY_raw; // int16_t from min to max range
+        int16_t gyroZ_raw; // int16_t from min to max range
+        float accel_raw_to_g;
+        float gyro_raw_to_dps;
+        // enum IMU_ACCEL_RANGE accel_range;
 
-    Serial.print("\t");
 
-    // Print rotation data
-    Serial.print("Rotation in deg/sec");
-    Serial.print("\t");
-    Serial.print("X: ");
-    Serial.print(imu.data.gyroX, 3);
-    Serial.print("\t");
-    Serial.print("Y: ");
-    Serial.print(imu.data.gyroY, 3);
-    Serial.print("\t");
-    Serial.print("Z: ");
-    Serial.println(imu.data.gyroZ, 3);
+}
+void getMotion(MPU6050& imu, int16_t )
+void loop() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
 
-    // Print 50x per second
-    delay(20);
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+        // Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+    #endif
+
+    #ifdef OUTPUT_BINARY_ACCELGYRO
+        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+    #endif
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
 }
