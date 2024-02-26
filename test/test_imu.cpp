1da1a10fda6d948c998b9adab3761098efef0872
ndelurgio <delurgion@gmail.com>
2024-02-25 20:54:56 -0800

    add imu bias calc

--- test/test_imu.cpp
+++ test/test_imu.cpp
@@ -2,6 +2,8 @@
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <Arduino.h>
+// #define DEBUG
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -13,14 +15,14 @@
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
-MPU6050 accelgyro;
+// MPU6050 accelgyro;
 //MPU6050 accelgyro(0x69); // <-- use for AD0 high
 //MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+// int16_t ax, ay, az;
+// int16_t gx, gy, gz;
 
-#define OUTPUT_IMU_SETTINGS
+// #define OUTPUT_IMU_SETTINGS
 
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
@@ -39,6 +41,113 @@ int16_t gx, gy, gz;
 #define LED_PIN 13
 bool blinkState = false;
 
+class IMU : public MPU6050 {
+    public:
+        void initialize() {
+            MPU6050::initialize();
+            update_config();
+        }
+        void update_config() {
+            int8_t accel_setting = getFullScaleAccelRange();
+            switch (accel_setting) {
+                case 0: //+-2g
+                    accel_g_to_raw = 16384.;
+                    accel_raw_to_g = 1./16384.;
+                    break;
+                case 1: //+-4g
+                    accel_g_to_raw = 8192.;
+                    accel_raw_to_g = 1./8192.;
+                    break;
+                case 2: //+-8g
+                    accel_g_to_raw = 4096.;
+                    accel_raw_to_g = 1./4096.;
+                    break;
+                case 3: //+-16g
+                    accel_g_to_raw = 2048.;
+                    accel_raw_to_g = 1./2048.;
+                    break;
+            }
+            int8_t gyro_setting = getFullScaleGyroRange();
+            switch (gyro_setting) {
+                case 0: //+-250deg/s
+                    gyro_dps_to_raw = 131.;
+                    gyro_raw_to_dps = 1./131.;
+                    break;
+                case 1: //+-500deg/s
+                    gyro_dps_to_raw = 65.5;
+                    gyro_raw_to_dps = 1./65.5;
+                    break;
+                case 2: //+-1000deg/s
+                    gyro_dps_to_raw = 32.8;
+                    gyro_raw_to_dps = 1./32.8;
+                    break;
+                case 3: //+-2000deg/s
+                    gyro_dps_to_raw = 16.4;
+                    gyro_raw_to_dps = 1./16.4;
+                    break;
+            }
+        }
+        void update_measurement() {
+            getMotion6(&accelX_raw,&accelY_raw,&accelZ_raw,&gyroX_raw,&gyroY_raw,&gyroZ_raw);
+            accelX = accelX_raw * accel_raw_to_g * g_to_m - accelX_bias;
+            accelY = accelY_raw * accel_raw_to_g * g_to_m - accelY_bias;
+            accelZ = accelZ_raw * accel_raw_to_g * g_to_m - accelZ_bias;
+            gyroX = gyroX_raw * gyro_raw_to_dps * deg_to_rad - gyroX_bias;
+            gyroY = gyroY_raw * gyro_raw_to_dps * deg_to_rad - gyroY_bias;
+            gyroZ = gyroZ_raw * gyro_raw_to_dps * deg_to_rad - gyroZ_bias;
+            #ifdef DEBUG
+                Serial.print("a/g:\t");
+                Serial.print(accelX_raw); Serial.print("\t");
+                Serial.print(accelY_raw); Serial.print("\t");
+                Serial.print(accelZ_raw); Serial.print("\t");
+                Serial.print(gyroX_raw); Serial.print("\t");
+                Serial.print(gyroY_raw); Serial.print("\t");
+                Serial.print(gyroZ_raw); Serial.print("\t");
+                Serial.print(accel_g_to_raw); Serial.print("\t");
+                Serial.print(gyro_dps_to_raw); Serial.print("\t");
+                Serial.print(getFullScaleAccelRange()); Serial.print("\t");
+                Serial.println(getFullScaleGyroRange());
+            #endif
+        }
+        void calibrate() {
+            update_measurement();
+            accelX_bias = accelX;
+            accelY_bias = accelY;
+            accelZ_bias = accelZ;
+            gyroX_bias = gyroX;
+            gyroY_bias = gyroY;
+            gyroZ_bias = gyroZ;
+        }
+        float accelX;       // meters/sec^2
+        float accelX_bias;  //meters/sec^2
+        float accelY;       // meters/sec^2
+        float accelY_bias;  //meters/sec^2
+        float accelZ;       // meters/sec^2
+        float accelZ_bias;  //meters/sec^2
+        float gyroX;        // rad/sec
+        float gyroX_bias;   // rad/sec
+        float gyroY;        // rad/sec
+        float gyroY_bias;   // rad/sec
+        float gyroZ;        // rad/sec
+        float gyroZ_bias;   // rad/sec
+    private:
+        const float g_to_m = 9.81;
+        const float deg_to_rad = PI/180.;
+        int16_t accelX_raw; // int16_t from min to max range
+        int16_t accelY_raw; // int16_t from min to max range
+        int16_t accelZ_raw; // int16_t from min to max range
+        int16_t gyroX_raw; // int16_t from min to max range
+        int16_t gyroY_raw; // int16_t from min to max range
+        int16_t gyroZ_raw; // int16_t from min to max range
+        float accel_raw_to_g;
+        float accel_g_to_raw;
+        float gyro_raw_to_dps;
+        float gyro_dps_to_raw;
+        // enum IMU_ACCEL_RANGE accel_range;
+};
+
+// IMU mpu6050 = IMU(accelgyro);
+IMU accelgyro;
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -52,115 +161,30 @@ void setup() {
     // it's really up to you depending on your project)
     Serial.begin(9600);
 
-    // initialize device
-    // Serial.println("Initializing I2C devices...");
+    // // initialize device
+    Serial.println("Initializing I2C devices...");
     accelgyro.initialize();
+    accelgyro.calibrate();
 
-    // verify connection
+    // // verify connection
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-    // use the code below to change accel/gyro offset values
-    /*
-    Serial.println("Updating internal sensor offsets...");
-    // -76	-2359	1688	0	0	0
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    accelgyro.setXGyroOffset(220);
-    accelgyro.setYGyroOffset(76);
-    accelgyro.setZGyroOffset(-85);
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    */
 
     // configure Arduino LED pin for output
     pinMode(LED_PIN, OUTPUT);
 
-    #ifdef OUTPUT_IMU_SETTINGS
-        Serial.println(accelgyro.getFullScaleAccelRange());
-        Serial.println(accelgyro.getFullScaleGyroRange());
-    #endif
 }
-
-
-// enum IMU_ACCEL_RANGE {
-//     PLUSMINUS_2G,
-//     PLUSMINUS_4G,
-//     PLUSMINUS_8G,
-//     PLUSMINUS_16G,
-// }
-class IMU {
-    public:
-        IMU(MPU6050 imu, float g = 9.81) {
-            g = g;
-            update_config(imu);
-        }
-        void update_config(MPU6050 imu) {
-            int8_t accel_config = imu.getFullScaleAccelRange();
-            switch (accel_config) {
-                case 0: //+-2g
-                    accel_raw_to_g = 1/16384.;
-                case 1: //+-4g
-                    accel_raw_to_g = 1/8192.;
-                case 2: //+-8g
-                    accel_raw_to_g = 1/4096.;
-                case 3: //+-16g
-                    accel_raw_to_g = 1/2048.;
-            }
-            int8_t gyro_config = imu.getFullScaleGyroRange();
-            switch (gyro_config) {
-                case 0: //+-250deg/s
-                    gyro_raw_to_dps = 1/131.;
-                case 1: //+-500deg/s
-                    gyro_raw_to_dps = 1/65.5;
-                case 2: //+-1000deg/s
-                    gyro_raw_to_dps = 1/32.8;
-                case 3: //+-2000deg/s
-                    gyro_raw_to_dps = 1/16.4;
-            }
-        }
-        void update_measurement() {
-            
-        }
-        float accelX; // meters/sec^2
-        float accelY; // meters/sec^2
-        float accelZ; // meters/sec^2
-        float gyroX;  // rad/sec
-        float gyroY;  // rad/sec
-        float gyroZ;  // rad/sec
-    private:
-        MPU6050 imu;
-        const float g = 9.81;
-        int16_t accelX_raw; // int16_t from min to max range
-        int16_t accelY_raw; // int16_t from min to max range
-        int16_t accelZ_raw; // int16_t from min to max range
-        int16_t gyroX_raw; // int16_t from min to max range
-        int16_t gyroY_raw; // int16_t from min to max range
-        int16_t gyroZ_raw; // int16_t from min to max range
-        float accel_raw_to_g;
-        float gyro_raw_to_dps;
-        // enum IMU_ACCEL_RANGE accel_range;
-
-
-}
-void getMotion(MPU6050& imu, int16_t )
 void loop() {
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
-
+    accelgyro.update_measurement();
+    #ifndef DEBUG
+        Serial.print("a/g:\t");
+        Serial.print(accelgyro.accelX); Serial.print("\t");
+        Serial.print(accelgyro.accelY); Serial.print("\t");
+        Serial.print(accelgyro.accelZ); Serial.print("\t");
+        Serial.print(accelgyro.gyroX); Serial.print("\t");
+        Serial.print(accelgyro.gyroY); Serial.print("\t");
+        Serial.println(accelgyro.gyroZ);
+    #endif
     #ifdef OUTPUT_READABLE_ACCELGYRO
         // display tab-separated accel/gyro x/y/z values
         // Serial.print("a/g:\t");
