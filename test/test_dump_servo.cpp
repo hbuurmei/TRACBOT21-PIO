13cccbaac0d9c7bf0fdf385c120f3fa483ba8a74
holdheim <holdheim@stanford.edu>
2024-02-29 12:08:18 -0800

    updated dumper servo code

--- test/test_dump_servo.cpp
+++ test/test_dump_servo.cpp
@@ -1,15 +1,45 @@
 #define SERVO_CTRL_PIN  3
+#define CLOSE   0   //for dumper hatch servo
+// #define RELEASE 360 //for dumper hatch servo
+#define RELEASE 80
+
+#define IN1     13
+#define IN2     12
+#define IN3     8
+#define IN4     7
+#define EnA     11
+#define EnB     6
+
 #include <Arduino.h>
 #include <Servo.h>
 
 Servo my_servo;
 void setup() {
+    //turn off motors
+    Serial.begin(9600);
+    pinMode(IN1,OUTPUT);
+    pinMode(IN2,OUTPUT);
+    pinMode(IN3,OUTPUT);
+    pinMode(IN4,OUTPUT);
+    pinMode(EnA,OUTPUT);
+    pinMode(EnB,OUTPUT);
+    // Configure Motor 1 (LEFT)
+    digitalWrite(IN1,LOW);  // HIGH = Forward, LOW = Backward
+    digitalWrite(IN2,LOW);  // Opposite of IN1
+    analogWrite(EnA,200);
+    // Configure Motor 2 (RIGHT)
+    digitalWrite(IN3,LOW);  // LOW = Forward, HIGH = Backward
+    digitalWrite(IN4,LOW);  // Opposite of IN3
+    analogWrite(EnB,200);
+
     // put your setup code here, to run once:
     pinMode(SERVO_CTRL_PIN,OUTPUT);
     // analogWrite(SERVO_CTRL_PIN,50);
     
     my_servo.attach(SERVO_CTRL_PIN);
-    my_servo.write(180);
+    my_servo.write(RELEASE);
+    delay(3000);
+    my_servo.write(CLOSE);
     
 }
 
---
+++ test/test_dynamic_braking.cpp
@@ -0,0 +1,94 @@
+#define IN1     13
+#define IN2     12
+#define IN3     8
+#define IN4     7
+#define EnA     11
+#define EnB     6
+#define DEFAULT_MOTOR_SPEED 4*PI // rad/s
+#define MAX_MOTOR_SPEED 20.9440 // rad/s
+#define RPS_TO_ANALOG 256 / MAX_MOTOR_SPEED
+#define WHEEL_RADIUS 0.042 // metets
+#define BASE_WIDTH 0.3048
+#define USE_TIMER_1         true
+#define CONTROLLER_FREQ 25.
+
+#include <Arduino.h>
+#include <sensors/imu.cpp>
+#include <TimerInterrupt.h>
+
+IMU imu;
+enum MODE {
+    STOP,
+    FORWARD
+};
+MODE curr_mode = FORWARD;
+void handle_mode(MODE mode);
+void controller();
+void controller2();
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    handle_mode(curr_mode);
+    imu.initialize();
+    imu.calibrate();
+    ITimer1.init();
+    ITimer1.setFrequency(CONTROLLER_FREQ, controller);
+}
+
+void handle_mode(MODE mode) {
+    switch(mode) {
+        case STOP:
+            // Left Motor
+            digitalWrite(IN1,LOW);
+            digitalWrite(IN2,LOW);
+            analogWrite(EnA,0);
+            // Right Motor
+            digitalWrite(IN3,LOW);
+            digitalWrite(IN4,LOW);
+            analogWrite(EnB,0);
+        case FORWARD:
+            // Left Motor
+            digitalWrite(IN1,HIGH);
+            digitalWrite(IN2,LOW);
+            analogWrite(EnA,DEFAULT_MOTOR_SPEED * RPS_TO_ANALOG);
+            // Right Motor
+            digitalWrite(IN3,LOW);
+            digitalWrite(IN4,HIGH);
+            analogWrite(EnB,DEFAULT_MOTOR_SPEED * RPS_TO_ANALOG);
+    }
+}
+float wz;
+float dw;
+float wr_cmd;
+void loop() {   
+    imu.update_measurement();
+    Serial.print("wz: ");Serial.print(wz);Serial.print("\t\t");
+    Serial.print("wr_cmd: ");Serial.println(wr_cmd);
+}
+
+float kp = 2.;
+float ki = 0.5;
+void controller() {
+    static float iwz = 0;
+    wz = imu.gyroZ;
+    // if (abs(wz) > 0.01) {
+    dw = -(kp*wz + ki*iwz)*BASE_WIDTH/WHEEL_RADIUS;
+    wr_cmd = dw + DEFAULT_MOTOR_SPEED;
+    analogWrite(EnB,wr_cmd * RPS_TO_ANALOG);
+    iwz += wz/CONTROLLER_FREQ;
+    // } else {Serial.print("\t");}
+}
+
+void controller2() {
+    static float yi_1 = 0;
+    static float yi_2 = 0;
+    static float ui_1 = 0;
+    static float ui_2 = 0;
+    dw = 0.03764*yi_1 - 0.03764*yi_2 + 1.879*ui_1 - 0.8869*ui_2;
+    wr_cmd = dw*BASE_WIDTH/WHEEL_RADIUS + DEFAULT_MOTOR_SPEED;
+    analogWrite(EnB,wr_cmd * RPS_TO_ANALOG);
+    yi_2 = yi_1;
+    yi_1 = imu.gyroZ;
+    ui_2 = ui_1;
+    ui_1 = dw;
+}
