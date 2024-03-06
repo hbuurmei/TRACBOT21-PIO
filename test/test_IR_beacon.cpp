b0b5c6b53399a3ad6b5001c71c6040c5f4e9612e
rcollins <robertcollins130@gmail.com>
2024-03-05 19:53:33 -0800

    discrete-angle testing code

--- test/test_IR_beacon.cpp
+++ test/test_IR_beacon.cpp
@@ -5,14 +5,14 @@
 #include <sensors/ir_beacon.cpp>
 #include <TimerInterrupt.h>
 
-#define ANGLE_INCREMENT PI/8
-#define MEASURE_DELAY 200
+#define ANGLE_INCREMENT PI/13
+#define MEASURE_DELAY 500
 
 IMU imu;
 IR_Beacon ir;
 
 float angle_target = 0; 
-long state_change;
+unsigned long state_change = 0;
 
 void next_angle();
 void take_measurement();
@@ -40,47 +40,22 @@ void loop() {
     imu.update_measurement();
     ir.update(imu.angZ);
     state();
-
-
-    if(imu.angZ < angle_target){
-        turn_left(MIDDLE, 1.7*PI);
-    }
-    else{
-        stop();
-        Serial.print(">ANGLE:");
-        Serial.println(imu.angZ);
-        Serial.print(">RAW:");
-        Serial.println(ir.raw);
-        Serial.print(">MAP:");
-        Serial.println(ir.value);
-        Serial.print(">MAV:");
-        Serial.println(ir.mav);
-    }
-
-    // Serial.print(">ANGLE:");
-    // Serial.println(imu.angZ);
-    // Serial.print(">RAW:");
-    // Serial.println(ir.raw);
-    // Serial.print(">MAP:");
-    // Serial.println(ir.value);
-    // Serial.print(">MAV:");
-    // Serial.println(ir.mav);
 }
 
 void next_angle(){
-    // if(imu.angZ < angle_target){
-    if (millis()-state_change < 1000){
-        turn_left(MIDDLE, 1.5*PI);
-    }
-    else{
+    if(imu.angZ >= angle_target){
+    //if (millis() > state_change + 1000){
         state=take_measurement;
         stop();
         state_change= millis();
     }
+    else{
+        turn_left(MIDDLE, 1.7*PI);
+    }
 }
 
 void take_measurement(){
-    if (millis()-state_change > MEASURE_DELAY){
+    if (millis() - state_change > MEASURE_DELAY){
         Serial.print(">ANGLE:");
         Serial.println(imu.angZ);
         Serial.print(">RAW:");
